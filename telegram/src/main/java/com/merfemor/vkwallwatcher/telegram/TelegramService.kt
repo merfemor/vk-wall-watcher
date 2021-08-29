930a536b24851e77c88bb9066b8a6e483aff503f
merfemor <efremov.roman98@gmail.com>
2021-08-30 01:41:56 +0300

    Start bot session

--- telegram/src/main/java/com/merfemor/vkwallwatcher/telegram/TelegramService.kt
+++ telegram/src/main/java/com/merfemor/vkwallwatcher/telegram/TelegramService.kt
@@ -2,11 +2,15 @@ package com.merfemor.vkwallwatcher.telegram
 
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import org.telegram.telegrambots.meta.TelegramBotsApi
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
 
 @Service
 internal class TelegramService(bot: Bot) {
     init {
-        logger.info("Started bot ${bot.botUsername}")
+        val api = TelegramBotsApi(DefaultBotSession::class.java)
+        api.registerBot(bot)
+        logger.info("Started bot session")
     }
 
     private companion object {
