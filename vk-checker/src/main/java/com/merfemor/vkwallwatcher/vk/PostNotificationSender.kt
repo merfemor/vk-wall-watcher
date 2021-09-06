8da7ccdf86d85786cfc6f3a61512086f0478ec9f
Roman Efremov <efremovr@ae.com>
2021-09-06 18:37:50 +0300

    Support telegram message limit, split to several messages

--- vk-checker/src/main/java/com/merfemor/vkwallwatcher/vk/PostNotificationSender.kt
+++ vk-checker/src/main/java/com/merfemor/vkwallwatcher/vk/PostNotificationSender.kt
@@ -2,6 +2,7 @@ package com.merfemor.vkwallwatcher.vk
 
 import com.merfemor.vkwallwatcher.data.VkWallWatchSubscription
 import com.merfemor.vkwallwatcher.telegram.TelegramMessageApi
+import com.vk.api.sdk.objects.groups.responses.GetByIdLegacyResponse
 import com.vk.api.sdk.objects.wall.WallpostFull
 import org.springframework.stereotype.Component
 
@@ -21,37 +22,63 @@ internal class PostNotificationSender(
         private val vkApi: VkApi,
         private val telegramMessageApi: TelegramMessageApi
 ) {
-    private fun constructMessage(communityId: Int, query: String, posts: Collection<WallpostFull>): String {
-        val limitedPosts = posts.take(MAX_POSTS_IN_MESSAGE) // TODO: handle telegram max message size instead of dropping posts
+    private fun constructPostString(postIndex: Int, communityInfo: GetByIdLegacyResponse, post: WallpostFull): String {
+        val ellipsizedText = post.text.ellipsize(MAX_TEXT_LENGTH)
+        val linkToPost = vkApi.getLinkForPost(communityInfo.screenName, communityInfo.id, post.id)
+        return POST_ROW_FMT.format(postIndex + 1, ellipsizedText, linkToPost)
+    }
 
-        val communityInfo = vkApi.getGroupInfoById(communityId)
-        val postListStr = limitedPosts.mapIndexed { index, post ->
-            val ellipsizedText = post.text.ellipsize(MAX_TEXT_LENGTH)
-            val linkToPost = vkApi.getLinkForPost(communityInfo.screenName, communityInfo.id, post.id)
-            POST_ROW_FMT.format(index + 1, ellipsizedText, linkToPost)
-        }.joinToString(separator = "\n\n")
-
-        val postsNumberString = if (limitedPosts.size < posts.size) {
-            "${posts.size} (showing only ${limitedPosts.size})"
+    private fun constructMessages(communityId: Int, query: String,
+                                  posts: Collection<WallpostFull>): Sequence<String> = sequence {
+        val limitedPosts = posts.asSequence().take(MAX_POSTS_IN_MESSAGE)
+        val postsNumberString = if (posts.size > MAX_POSTS_IN_MESSAGE) {
+            "${posts.size} (showing only $MAX_POSTS_IN_MESSAGE)"
         } else {
             "${posts.size}"
         }
-        return NOTIFICATION_MESSAGE_FMT.format(postsNumberString, communityInfo.name, query, postListStr)
+
+        val communityInfo = vkApi.getGroupInfoById(communityId)
+
+        val postStrings = limitedPosts.mapIndexed { index, post ->
+            constructPostString(index, communityInfo, post)
+        }
+        val sb = StringBuilder()
+        val maxLengthForPostsString = TELEGRAM_MAX_MESSAGE_SIZE - NOTIFICATION_MESSAGE_FMT.length
+
+        fun constructFullMessageString(): String {
+            val postsString = sb.toString()
+            return NOTIFICATION_MESSAGE_FMT.format(postsNumberString, communityInfo.name, query, postsString)
+        }
+
+        val postsSeparator = "\n\n"
+
+        for (postString in postStrings) {
+            if (sb.length + postString.length + postsSeparator.length > maxLengthForPostsString) {
+                yield(constructFullMessageString())
+                sb.clear()
+            }
+            if (sb.isNotEmpty()) {
+                sb.append(postsSeparator)
+            }
+            sb.append(postString)
+        }
+        yield(constructFullMessageString())
     }
 
     fun notifyAboutNewPosts(subscription: VkWallWatchSubscription, posts: Collection<WallpostFull>) {
         if (posts.isEmpty()) {
             return
         }
-        val messageText = constructMessage(subscription.communityId, subscription.query, posts)
-        telegramMessageApi.sendTextMessage(subscription.chatId, messageText,
-                enableHtml = true, enabledWebPagePreview = false)
+        for (messageTextPart in constructMessages(subscription.communityId, subscription.query, posts)) {
+            telegramMessageApi.sendTextMessage(subscription.chatId, messageTextPart,
+                    enableHtml = true, enabledWebPagePreview = false)
+        }
     }
 
     private companion object {
         private const val TELEGRAM_MAX_MESSAGE_SIZE = 4096
         private const val MAX_TEXT_LENGTH = 300
-        private const val MAX_POSTS_IN_MESSAGE = 5
+        private const val MAX_POSTS_IN_MESSAGE = 30
 
         private fun String.ellipsize(maxLength: Int): String {
             if (length <= maxLength) {
