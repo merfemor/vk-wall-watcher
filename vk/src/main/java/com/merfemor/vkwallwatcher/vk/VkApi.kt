0d092c388116940c57a462a955c7bfdf73f667e1
Roman Efremov <efremovr@ae.com>
2021-09-06 16:38:41 +0300

    Add search for all vk posts with count/offset and date filter

--- vk/src/main/java/com/merfemor/vkwallwatcher/vk/VkApi.kt
+++ vk/src/main/java/com/merfemor/vkwallwatcher/vk/VkApi.kt
@@ -4,8 +4,9 @@ import com.vk.api.sdk.client.VkApiClient
 import com.vk.api.sdk.client.actors.ServiceActor
 import com.vk.api.sdk.httpclient.HttpTransportClient
 import com.vk.api.sdk.objects.groups.responses.GetByIdLegacyResponse
-import com.vk.api.sdk.objects.wall.responses.GetResponse
+import com.vk.api.sdk.objects.wall.WallpostFull
 import org.springframework.stereotype.Component
+import java.util.Date
 
 @Component
 class VkApi internal constructor(
@@ -22,11 +23,43 @@ class VkApi internal constructor(
         return getGroupInfoByNameId(nameId).id
     }
 
-    fun searchGroupWallPosts(groupId: Int, query: String, count: Int = 100, offset: Int = 0): GetResponse {
+    fun searchGroupAllWallPosts(groupId: Int, query: String,
+                                minDate: Date? = null, maxDate: Date? = null): Collection<WallpostFull> {
+        var offset = 0
+        val result = mutableListOf<WallpostFull>()
+        while (true) {
+            val items = searchGroupWallPostsWithCount(groupId, query, minDate, MAX_COUNT, offset)
+            result.addAll(items)
+            if (items.size < MAX_COUNT) {
+                break
+            }
+            offset += MAX_COUNT
+        }
+        if (maxDate == null) {
+            return result
+        }
+        return result.filter { Date(it.date.toLong()).before(maxDate) }
+    }
+
+    private fun searchGroupWallPostsWithCount(groupId: Int, query: String, minDate: Date? = null,
+                                              count: Int, offset: Int): Collection<WallpostFull> {
+        assert(count in 1..100) { "Count must be between 0 and 100, actual $count" }
+        assert(offset >= 0) { "Offset must be non-negative, actual $offset" }
+
         val id = -groupId // group ids with minuses
-        return client.wall().get(actor).ownerId(id)
+        val result = client.wall().search(actor).ownerId(id)
                 .count(count)
+                .query(query)
                 .offset(offset)
                 .execute()
+                .items
+        if (minDate == null) {
+            return result
+        }
+        return result.filter { Date(it.date.toLong()).after(minDate) }
+    }
+
+    private companion object {
+        private const val MAX_COUNT = 100
     }
 }
