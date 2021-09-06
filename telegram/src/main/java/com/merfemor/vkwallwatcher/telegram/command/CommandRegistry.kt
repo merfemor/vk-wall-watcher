bc7cffed31fd8dd524faed761f6db77afbe2238e
Roman Efremov <efremovr@ae.com>
2021-09-06 20:37:08 +0300

    Delete /help command

--- telegram/src/main/java/com/merfemor/vkwallwatcher/telegram/command/CommandRegistry.kt
+++ telegram/src/main/java/com/merfemor/vkwallwatcher/telegram/command/CommandRegistry.kt
@@ -6,13 +6,11 @@ import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand
 
 @Component
 internal class CommandRegistry(
-    helpCommand: HelpCommand,
     cancelCommand: CancelCommand,
     newWatchCommand: NewWatchCommand,
     deleteWatchCommand: DeleteWatchCommand
 ) {
     private val commands = arrayOf<IBotCommand>(
-        helpCommand,
         cancelCommand,
         newWatchCommand,
         deleteWatchCommand
