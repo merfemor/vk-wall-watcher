3071710c656f7f0be3971aae4736f757a9c2a3ba
Roman Efremov <efremov.roman98@gmail.com>
2021-09-06 23:00:27 +0300

    Validate properties

--- telegram/src/main/java/com/merfemor/vkwallwatcher/telegram/TelegramProperties.java
+++ telegram/src/main/java/com/merfemor/vkwallwatcher/telegram/TelegramProperties.java
@@ -1,25 +1,31 @@
 package com.merfemor.vkwallwatcher.telegram;
 
-import edu.umd.cs.findbugs.annotations.NonNull;
-import edu.umd.cs.findbugs.annotations.Nullable;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.validation.annotation.Validated;
+
+import javax.annotation.CheckForNull;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 /**
  * @implNote not works if written in Kotlin
  */
 @ConstructorBinding
 @ConfigurationProperties(prefix = "telegram")
+@Validated
 class TelegramProperties {
-    @NonNull
+    @NotNull
+    @NotEmpty
     final String botUsername;
-    @NonNull
+    @NotNull
+    @NotEmpty
     final String botToken;
-    @Nullable
+    @CheckForNull
     final String[] permittedUsernames;
 
-    TelegramProperties(@NonNull String botUsername, @NonNull String botToken,
-                       @Nullable String[] permittedUsernames) {
+    private TelegramProperties(String botUsername, String botToken,
+                               @CheckForNull String[] permittedUsernames) {
         this.botUsername = botUsername;
         this.botToken = botToken;
         this.permittedUsernames = permittedUsernames;
--- vk/src/main/java/com/merfemor/vkwallwatcher/vk/VkProperties.java
+++ vk/src/main/java/com/merfemor/vkwallwatcher/vk/VkProperties.java
@@ -1,22 +1,28 @@
 package com.merfemor.vkwallwatcher.vk;
 
-import edu.umd.cs.findbugs.annotations.NonNull;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @ConfigurationProperties(prefix = "vk")
 @ConstructorBinding
+@Validated
 public class VkProperties {
-    public final int appId;
-    @NonNull
+    @NotNull
+    public final Integer appId;
+    @NotNull
     public final String clientSecret;
-    @NonNull
+    @NotNull
     public final String accessToken;
-    @NonNull
+    @NotNull
+    @NotEmpty
     public final String checkScheduleCron;
 
-    private VkProperties(int appId, @NonNull String clientSecret, @NonNull String accessToken,
-                 @NonNull String checkScheduleCron) {
+    private VkProperties(Integer appId, String clientSecret, String accessToken, String checkScheduleCron) {
         this.appId = appId;
         this.clientSecret = clientSecret;
         this.accessToken = accessToken;
