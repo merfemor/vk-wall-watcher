7f9ff40550014faf39442a5817cb95e8a848a927
Roman Efremov <efremovr@ae.com>
2021-09-06 15:32:18 +0300

    Add created date into subscription

--- storage/src/main/java/com/merfemor/vkwallwatcher/data/VkWallWatchSubscription.java
+++ storage/src/main/java/com/merfemor/vkwallwatcher/data/VkWallWatchSubscription.java
@@ -1,8 +1,12 @@
 package com.merfemor.vkwallwatcher.data;
 
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.lang.NonNull;
+
+import java.util.Date;
 
 /**
  * @implNote mongo fails to create mapper if this class written in Kotlin
@@ -16,16 +20,21 @@ public final class VkWallWatchSubscription {
     public final int communityId;
     @NonNull
     public final String keywords;
+    @CreatedDate
+    @NonNull
+    public final Date createdDate;
 
     public VkWallWatchSubscription(long chatId, int communityId, @NonNull String keywords) {
-        this("", chatId, communityId, keywords);
+        this("", chatId, communityId, keywords, new Date());
     }
 
+    @PersistenceConstructor
     private VkWallWatchSubscription(@NonNull String id, long chatId, int communityId,
-                                   @NonNull String keywords) {
+                                    @NonNull String keywords, @NonNull Date createdDate) {
         this.id = id;
         this.chatId = chatId;
         this.communityId = communityId;
         this.keywords = keywords;
+        this.createdDate = createdDate;
     }
 }
