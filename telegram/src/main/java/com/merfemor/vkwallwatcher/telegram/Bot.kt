bc3583919ff19cd61b746c66313906c58381dc7c
Roman Efremov <efremovr@ae.com>
2021-10-21 15:11:43 +0300

    Fix NPE in bot

--- telegram/src/main/java/com/merfemor/vkwallwatcher/telegram/Bot.kt
+++ telegram/src/main/java/com/merfemor/vkwallwatcher/telegram/Bot.kt
@@ -2,6 +2,7 @@ package com.merfemor.vkwallwatcher.telegram
 
 import com.merfemor.vkwallwatcher.telegram.command.CallbackQueryProcessorHolder
 import com.merfemor.vkwallwatcher.telegram.filter.PermittedUsersMessagesFilterImpl
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery
@@ -26,6 +27,10 @@ internal class Bot(
             return
         }
         val message = update.message
+        if (message == null) {
+            log.info("Skip non-message and non callback update {}", message)
+            return
+        }
         if (!messagesFilter.test(message.from)) {
             messagesFilter.responseMessageForReject?.let {
                 sendHelper.sendTextMessage(message.chatId, this, it)
@@ -41,5 +46,9 @@ internal class Bot(
             return
         }
         callbackQueryProcessor.process(callbackQuery, this)
+    }
+
+    private companion object {
+        private val log = LoggerFactory.getLogger(Bot::class.java)
     }
 }
